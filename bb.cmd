ef78dc4076d9a5a14363b0a68403f8165c1551fe
madrahimova <darya.mad.ry@gmail.com>
2022-04-16 14:39:10 +0300

    Fixes for Windows

--- bb.cmd
+++ bb.cmd
@@ -1,1 +1,5 @@
-docker run --platform linux/i386 --name blackbox --rm -it -v "%cd%":/~ bb %*
+@echo off
+@setlocal enableDelayedExpansion
+set files=
+@for %%f in (%*) do @echo.%%f | findstr /R [^-] >nul && ( set files=!files! %%f) || ( set files=!files! /root/home/%%f )
+docker run --platform linux/i386 --name blackbox --rm -it -v "%cd%":/root/home bb %files%
