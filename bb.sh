475e5be519991fd078b54afa29bf20a2a6ed8974
madrahimova <31032981+madrahimova@users.noreply.github.com>
2022-04-18 20:56:13 +0300

    Fixes for macOS

--- bb.sh
+++ bb.sh
@@ -1,3 +1,4 @@
-files=`for f in "$@" ; do echo "$f" done`
+files=`for f in "$@" ; do echo "$f" ; done`
 dir=${PWD##*/}
+echo $files
 docker run --platform linux/i386 --name blackbox --rm -it -v "$PWD":"/$dir" -w "/$dir" bb $files
